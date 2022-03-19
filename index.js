79fe2100a478096477869e7298f94fe5b47458ee
sevstar2016 <45399728+sevstar2016@users.noreply.github.com>
2022-03-20 01:08:28 +0500

    Delete session directory
    tlapi
    
    .env
    
    .gitignore

--- index.js
+++ index.js
@@ -1,60 +1,14 @@
-const { Api, TelegramClient } = require("telegram")
-const { StoreSession } = require("telegram/sessions")
-const input = require("input")
-const fs = require('fs')
+const { tlapi } = require('./tlapi')
+require('dotenv').config()
 
-const appId = 7104260;
-const hash = '5168d523b6957d158b6b6928221b10c6'
-const session = new StoreSession('session')
+const appId = process.env.appId;
+const hash = process.env.hash
+const tlApi = new tlapi(appId, hash)
 
 async function start() {
-    console.log("Loading...")
-    const client = new TelegramClient(session, appId, hash, {
-      connectionRetries: 5,
-    })
-    // await client.start({
-    //   phoneNumber: async () => await input.text("Please enter your number: "),
-    //   phoneCode: async () => await input.text("Please enter the code you received: "),
-    //   onError: (err) => console.log(err),
-    // })
-    await client.session.load()
-    await client.connect()
-    console.log("connected.")
-    const result = await client.invoke(new Api.updates.GetState({}))
-    const q = await result
-    const r = await client.invoke(new Api.updates.GetDifference({
-        pts: 109900,
-        date: q.date,
-        qts: q.qts
-    }))
-
-    const json = await r.toJSON()
-
-    fs.writeFile('./users.json', JSON.stringify(json.users), err => {
-        if(err){
-            console.error(err)
-            return
-        }
-    })
-    fs.writeFile('./chats.json', JSON.stringify(json.newMessages), err => {
-        if(err){
-            console.error(err)
-            return
-        }
-    })
-
-    fs.writeFile('./delete.json', JSON.stringify(json.otherUpdates), err => {
-        if(err){
-            console.error(err)
-            return
-        }
-    })
-
-    console.log(await client.invoke(new Api.messages.GetMessages({
-        id: [51288,
-            51290,
-            51289]
-    })))
+    tlApi.load()
 }
 
-start()
+start().then(() => {
+    console.log("DONE!")
+})
---
+++ tlapi.js
@@ -0,0 +1,117 @@
+const { Api, TelegramClient } = require("telegram")
+const { StoreSession } = require("telegram/sessions")
+const input = require("input")
+const fs = require('fs');
+
+class tlapi {
+    constructor(appId, hash) {
+        this.session = new StoreSession('session')
+        console.log("Loading...")
+        this.client = new TelegramClient(this.session, appId, hash, {
+            connectionRetries: 5
+        })
+
+        this.diff
+        this.result
+        this.lpts
+    }
+
+    async login() {
+        await this.client.start({
+            phoneNumber: async() => await input.text("Please enter your number: "),
+            phoneCode: async() => await input.text("Please enter the code you received: "),
+            onError: (err) => console.log(err),
+        })
+        this.client.connect()
+        console.log("connected.")
+    }
+
+    async load() {
+        await this.client.session.load()
+        this.client.connect()
+        console.log("connected.")
+    }
+
+    async logInit() {
+        this.result = await this.client.invoke(new Api.updates.GetState({}))
+        this.diff = await this.client.invoke(new Api.updates.GetDifference({
+            pts: this.result.pts - 2,
+            date: this.result.date,
+            qts: this.result.qts,
+            ptsTotalLimit: 30
+        }))
+        this.lpts = this.result.pts - 2
+    }
+
+    async log() {
+        while (true) {
+            this.result = await this.client.invoke(new Api.updates.GetState({}))
+
+            if (this.lpts !== this.result.pts - 2) {
+                let json = await this.diff.toJSON()
+
+                fs.writeFile('./users.json', JSON.stringify(json.users), err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                })
+
+                fs.writeFile('./delete.json', JSON.stringify(json.otherUpdates), err => {
+                    if (err) {
+                        console.error(err)
+                        return
+                    }
+                })
+
+                let history = []
+                let messages = ''
+
+                fs.readFile('./history.json', 'utf-8', async(err, data) => {
+                    if (err) {
+                        return
+                    }
+                    try {
+                        history = JSON.parse(data)
+                        history.push(json.otherUpdates)
+                    } catch {
+                        history = json.otherUpdates
+                    }
+
+                    fs.writeFile('./history.json', JSON.stringify(history), err => {
+                        if (err) {
+                            console.error(err)
+                            return
+                        }
+                    })
+
+                    history.forEach(struct => {
+                        try {
+                            messages += struct.message.message + '\n'
+                        } catch {
+                            console.log(struct)
+                        }
+                    });
+                    fs.writeFile('./messages', messages, err => {
+                        if (err)
+                            return
+                    })
+                })
+
+
+                diff = await client.invoke(new Api.updates.GetDifference({
+                    pts: lpts,
+                    date: result.date,
+                    qts: result.qts,
+                    ptsTotalLimit: 30
+                }))
+                lpts += 1
+                console.log("Updated")
+            }
+        }
+    }
+}
+
+module.exports = {
+    tlapi
+}
